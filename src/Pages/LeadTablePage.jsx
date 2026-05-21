5fbcc42c3d6c03f7004b7fccd892895baf24944a
Raj <raj@Rajs-MacBook-Air.local>
2026-05-22 03:11:57 +0530

    Fixed import errors again

--- src/Pages/LeadTablePage.jsx
+++ src/Pages/LeadTablePage.jsx
@@ -1,4 +1,3 @@
-import react from "react";
 import LeadTable from "../Components/LeadTable";
 
 const leadsTable =() => {
--- src/Pages/TicketTablePage.jsx
+++ src/Pages/TicketTablePage.jsx
@@ -1,4 +1,3 @@
-import react from "react";
 import TicketTable from "../Components/TicketTable";
 
 const tickettable =() => {
